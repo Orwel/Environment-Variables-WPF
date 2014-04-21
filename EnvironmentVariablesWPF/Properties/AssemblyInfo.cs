dc04eeb592b3f221eee69cdf29b79454b3afb7ca
Vernou Cédric <cedric.vernou@gmail.com>
2014-04-21 17:02:04 +0200

    Environment Variable WPF 1.1

--- EnvironmentVariablesWPF/Properties/AssemblyInfo.cs
+++ EnvironmentVariablesWPF/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("EnvironmentVariablesWPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Alternative to edit environment variable")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ultimate Company")]
 [assembly: AssemblyProduct("EnvironmentVariablesWPF")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
